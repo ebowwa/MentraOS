400d607c61cffa2e321b2655aed71082674793ab
Loay Yari <loay@okrasolar.com>
2025-08-09 01:50:39 +0800

    fix: Remove build folder from git tracking
    - Removed entire build/ directory from version control
    - Build artifacts should never be committed to repository
    - .gitignore already properly excludes build/ directory
    - This resolves the accidental inclusion of local build files

--- mcu_client/nrf5340_ble_simulator/build/nrf5340_ble_simulator/modules/libmetal/libmetal/lib/compiler/armcc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /opt/nordic/ncs/v3.0.0/modules/hal/libmetal/libmetal/lib/compiler/armcc
-# Build directory: /Users/loayyari/Documents/Github/MentraOS/mcu_client/nrf5340_ble_simulator/build/nrf5340_ble_simulator/modules/libmetal/libmetal/lib/compiler/armcc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
