7f0872477b5fccda51e7dc6c84a07b5cb393105d
fossephate <matt@fosse.co>
2025-09-08 11:52:12 -0700

    fix-rust-build

--- rust/livekit/build_ios_framework.sh
+++ rust/livekit/build_ios_framework.sh
@@ -1,7 +1,17 @@
 
+# export IPHONEOS_DEPLOYMENT_TARGET=12.0
 cargo build --release --target aarch64-apple-ios
+# cargo build --release --target aarch64-apple-ios-sim
+
+# xcodebuild -create-xcframework \
+#   -library ./target/aarch64-apple-ios/release/liblivekit_bridge.dylib \
+#   -headers ./include/ \
+#   -library ./target/aarch64-apple-ios-sim/release/liblivekit_bridge.dylib \
+#   -headers ./include/ \
+#   -output ios/LiveKit.xcframework
+
 
 xcodebuild -create-xcframework \
-  -library ../target/aarch64-apple-ios/release/libmobile.dylib \
+  -library ./target/aarch64-apple-ios/release/liblivekit_bridge.dylib \
   -headers ./include/ \
-  -output ios/MobileExample.xcframework
+  -output ios/LiveKit.xcframework
